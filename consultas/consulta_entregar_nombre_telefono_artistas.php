06ff713071f091067871d88c02d5f4cea95c60b1
IgnacioHqz <iehenriquez@uc.cl>
2022-11-07 19:22:28 -0300

    agregando primera consulta ihn

---
+++ consultas/consulta_entregar_nombre_telefono_artistas.php
@@ -0,0 +1,28 @@
+#Ignacio Henriquez Novoa 07-11-2022 cita: video ayudantia semestres pasado
+<?php include('../templatesihn/header.html');   ?>
+
+<body>
+<?php   
+    require("../config/conexion.php");
+    $query = "SELECT nombre, n_contacto FROM productoras";
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $productoras = $result -> fetchAll();
+
+    #print_r($productora);
+
+
+?>
+<table>
+    <tr>
+        <th>Nombre Productora</th>
+        <th>Número de teléfono</th>
+    </tr>
+
+    <?php
+    foreach($productoras as $productora){
+        echo "<tr><td>$productora[0]</td><td>$productora[1]</td></tr>";
+    }>
+</table>
+
+<?php include("../templatesihn/footer.html"); ?>
--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <!--Ignacio Henriquez Novoa 07-11-2022-->
 
-<?php include('templates/header.html'); ?>
+<?php include('templatesihn/header.html'); ?>
 <body>
-    <h1 align="center"> Ignacio </h1>
+    <h1 align="center"> Entrega 2 Bases de datos </h1>
+    <h3 align="center"> Verónica Baeza - Ignacio Henríquez Novoa </h3>
+    <br>
+    <br>
+    <h3 align="center"> Entregar un listado del nombre y teléfono de contacto de todos los artistas </h3>
+    <form align="center" action="consultas/consultas_entregar_nombre_telefono_artistas.php" method="post">
+        <input type="submit" value="Buscar">
+    <h3 align="center"> Para conocer el número de entradas de cortesía que ha entregado un artista, 
+        introduzca el nombre abajo </h3>
+    <h3 align="center"> Para conocer los datos del último tour (el más reciente) de un artista, 
+        introduzca el nombre abajo </h3>
+    <h3 align="center"> Para conocer los países que serán visitados en un tour, introduzca el 
+        nombre del tour abajo </h3>
+    <h3 align="center"> Para conocer todas las productoras con las que ha trabajado un artista, 
+        introduzca el nombre del artista abajo </h3>
+    <h3 align="center"> Para conocer todos los hoteles en los que se ha quedado un artista, y 
+        cuántas veces se ha hospedado en cada uno, introduzca el nombre del artista abajo </h3> 
+    <h3 align="center">Para mostrar al artista que ha entregado la mayor cantidad de entradas de 
+        contesía, haga clic en el boton de abajo </h3>
+
     <p style ="text-align:center;"> Hola xd </p>
 </body>
 </html>
