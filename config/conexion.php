b1c5ee58f27326a26ed8255fd4d1b9381ce98a02
IgnacioHqz <iehenriquez@uc.cl>
2022-11-07 16:39:49 -0300

    subiendo archivos para ver si funciona página

---
+++ config/conexion.php
@@ -0,0 +1,10 @@
+#Ignacio Henriquez Novoa 07-11-2022
+
+<?php
+    try {
+        requiere('data.php');
+        $db = new PDO ("psgql:dbname="$databaseName";host=localhost;port=5432;$user;password=$password);
+    } catch (Exception $e) {
+        echo "No se pudo conectar a la base de datos: $e";
+    }
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<!--Ignacio Henriquez Novoa 07-11-2022-->
+
+<?php include('templates/header.html'); ?>
+<body>
+    <h1 align="center"> Ignacio </h1>
+    <p style ="text-align:center;"> Hola xd </p>
+</body>
+</html>
