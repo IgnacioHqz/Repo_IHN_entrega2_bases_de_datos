bd213cc8b325cacad9895c7f7cb0c131cb117695
IgnacioHqz <iehenriquez@uc.cl>
2022-11-08 02:52:03 -0300

    arreglando corchetes c5

--- consultas/consulta_productoras_que_ha_trabajado_artista.php
+++ consultas/consulta_productoras_que_ha_trabajado_artista.php
@@ -5,7 +5,7 @@
 <body>
     <?php
     require("../config/conexion.php");
-    $var = $_POST("artista");
+    $var = $_POST["artista"];
     $query = "SELECT * FROM eventos LEFT JOIN artistas ON eventos.aid=artistas.aid;"; #cita ihn: COLOCAR!
     #$query_productoras_que_trabajo_artista = "SELECT DISTINCT pid FROM $query WHERE $query.nombre_artistico='$var';";
     #$result = $db -> prepare($query_productoras_que_trabajo_artista);
@@ -43,4 +43,5 @@
     ?>
     </table>
 
-<?php include('--7templatesihn/footer.html'); ?>
+<?php include('--templatesihn/footer.html'); ?>
+
