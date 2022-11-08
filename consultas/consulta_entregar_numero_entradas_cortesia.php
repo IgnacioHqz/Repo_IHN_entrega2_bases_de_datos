b8f7011fbfdbd672f8476e7d8a0651a9241f19eb
IgnacioHqz <iehenriquez@uc.cl>
2022-11-08 20:20:58 -0300

    arreglando consulta 2 (colocandole print_r para imprimir resultado count)

--- consultas/consulta_entregar_numero_entradas_cortesia.php
+++ consultas/consulta_entregar_numero_entradas_cortesia.php
@@ -2,6 +2,9 @@
 <?php include('../templatesihn/header.html');   ?>
 
 <body>
+
+
+
     <?php
     require("../config/conexion.php");
 
@@ -10,18 +13,12 @@
     $query = "SELECT COUNT(DISTINCT tablaIHN.aid) FROM (SELECT * FROM entradas_cortesia, artistas WHERE entradas_cortesia.aid = artistas.aid) AS tablaIHN WHERE tablaIHN.nombre_artistico='$var';";
     $result = $db -> prepare($query);
     $result -> execute();
-    $dataCollected = $result -> fetchAll();
-    print_r($dataCollected)
+    
+    #$dataCollected = $result -> fetchAll();
+    print_r($result->fetchColumn());
     ?>
 
-    <table>
-        <tr>
-            <th>Número de entradas de cortesía que se ha entregado</th>
-        </tr>
-    <?php
-    foreach ($dataCollected as $p) {
-        echo "<tr> <td>$p[0]</td> </tr>";
-    }
-    ?>
+
+
 
 <?php include('../templatesihn/footer.html'); ?>
