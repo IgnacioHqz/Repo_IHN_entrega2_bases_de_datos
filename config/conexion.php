a4a1f1db7c1b96f1c9116b788b5b044b3d092e87
IgnacioHqz <iehenriquez@uc.cl>
2022-11-07 21:58:52 -0300

    arreglando requiere mal escrito

--- config/conexion.php
+++ config/conexion.php
@@ -2,7 +2,7 @@
 
 <?php
     try {
-        requiere('data.php');
+        require('data.php');
         $db = new PDO("psgql:dbname=$databaseName;host=localhost;port=5432;$user;password=$password");
     } catch (Exception $e) {
         echo "No se pudo conectar a la base de datos: $e";
