282ae64a88ff37d4015392d5a37e2e22f27e520e
IgnacioHqz <iehenriquez@uc.cl>
2022-11-07 21:57:09 -0300

    agregando una comilla que faltaba a consulta 2

--- config/conexion.php
+++ config/conexion.php
@@ -3,7 +3,7 @@
 <?php
     try {
         requiere('data.php');
-        $db = new PDO ("psgql:dbname="$databaseName";host=localhost;port=5432;$user;password=$password);
+        $db = new PDO("psgql:dbname=$databaseName;host=localhost;port=5432;$user;password=$password");
     } catch (Exception $e) {
         echo "No se pudo conectar a la base de datos: $e";
     }
