65ebc31dbc49e1379b5b3d1cb7d18d6bf7c38ca7
IgnacioHqz <iehenriquez@uc.cl>
2022-11-08 18:38:17 -0300

    agregando un ; en el archivo consulta

--- consultas/consulta_entregar_numero_entradas_cortesia.php
+++ consultas/consulta_entregar_numero_entradas_cortesia.php
@@ -5,7 +5,7 @@
     <?php
     require("../config/conexion.php");
 
-    $var = $_POST["artista"]
+    $var = $_POST["artista"];
     $query = "SELECT COUNT(DISTINCT *) FROM entradas_cortesia WHERE aid = (SELECT artistas.aid FROM artistas WHERE nombre_artistico='$var');";
     $result = $db -> prepare($query);
     $result -> execute();
