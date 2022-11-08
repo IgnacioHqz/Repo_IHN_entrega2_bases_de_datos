93d42187ef0cb1e559cf90bd803f0debda2bb21c
IgnacioHqz <iehenriquez@uc.cl>
2022-11-08 19:01:32 -0300

    haciendo de nuevo consulta 2

--- consultas/consulta_entregar_numero_entradas_cortesia.php
+++ consultas/consulta_entregar_numero_entradas_cortesia.php
@@ -6,7 +6,8 @@
     require("../config/conexion.php");
 
     $var = $_POST["artista"];
-    $query = "SELECT COUNT(DISTINCT *) FROM entradas_cortesia WHERE aid = (SELECT artistas.aid FROM artistas WHERE nombre_artistico='$var');";
+    #$query = "SELECT COUNT(DISTINCT *) FROM entradas_cortesia WHERE aid = (SELECT artistas.aid FROM artistas WHERE nombre_artistico='$var');"; IHN: no funciono
+    $query = "SELECT COUNT(DISTINCT *) FROM (SELECT * FROM entradas_cortesia, artistas WHERE entradas_cortesia.aid = artistas.aid) AS tablaIHN WHERE tablaIHN.nombre_artistico='$var';";
     $result = $db -> prepare($query);
     $result -> execute();
     $dataCollected = $result -> fetchAll();
