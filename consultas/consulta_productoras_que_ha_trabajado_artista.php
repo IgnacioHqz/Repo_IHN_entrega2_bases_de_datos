558cb80ed1128a8aa8c8472dc61063a0704ed41c
IgnacioHqz <iehenriquez@uc.cl>
2022-11-08 09:40:00 -0300

    print_r

--- consultas/consulta_productoras_que_ha_trabajado_artista.php
+++ consultas/consulta_productoras_que_ha_trabajado_artista.php
@@ -7,29 +7,26 @@
     require("../config/conexion.php");
     $var = $_POST["artista"];
     $query = "SELECT * FROM eventos LEFT JOIN artistas ON eventos.aid=artistas.aid;"; #cita ihn: COLOCAR!
-    #$query_productoras_que_trabajo_artista = "SELECT DISTINCT pid FROM $query WHERE $query.nombre_artistico='$var';";
-    #$result = $db -> prepare($query_productoras_que_trabajo_artista);
-    #$result -> execute();
-    #$dataCollected = $result -> fetchAll();
 
     $result = $db -> prepare($query);
     $result -> execute();
-    #$dataCollected = $result -> fetchAll();
-
-    $query_productoras_que_trabajo_artista = "SELECT DISTINCT pid FROM $result WHERE $result.nombre_artistico='$var';";
-    $result = $db -> prepare($query_productoras_que_trabajo_artista);
-    $result -> execute();
-    #$dataCollected = $result -> fetchAll();
-
-    $query_join_entre_obtenido_y_tabla_productora = "SELECT * FROM $result LEFT JOIN productoras ON $result.pid=productoras.pid;";
-    $result = $db -> prepare($query_join_entre_obtenido_y_tabla_productora);
-    $result -> execute();
-    #$dataCollected = $result -> fetchAll();
-
-    $query_para_obtener_productoras = "SELECT DISTINCT nombre FROM $result;";
-    $result = $db -> prepare($query_para_obtener_productoras);
-    $result -> execute();
     $dataCollected = $result -> fetchAll();
+    print_r($dataCollected)
+
+#    $query_productoras_que_trabajo_artista = "SELECT DISTINCT pid FROM $dataCollected WHERE $.nombre_artistico='$var';";
+#    $result = $db -> prepare($query_productoras_que_trabajo_artista);
+#    $result -> execute();
+    
+
+#    $query_join_entre_obtenido_y_tabla_productora = "SELECT * FROM $result LEFT JOIN productoras ON $result.pid=productoras.pid;";
+#    $result = $db -> prepare($query_join_entre_obtenido_y_tabla_productora);
+#    $result -> execute();
+    
+
+#    $query_para_obtener_productoras = "SELECT DISTINCT nombre FROM $result;";
+#    $result = $db -> prepare($query_para_obtener_productoras);
+#    $result -> execute();
+#    $dataCollected = $result -> fetchAll();
     ?>
 
     <table>
